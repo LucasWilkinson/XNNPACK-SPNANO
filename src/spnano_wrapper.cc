6f092b41657724f256076832f5014eb5ab189916
Lucas Wilkinson <wilkinson.lucas@gmail.com>
2022-10-12 14:46:17 -0400

    Clean up spnano wrapper

--- src/spnano_wrapper.cc
+++ src/spnano_wrapper.cc
@@ -31,110 +31,6 @@
 #include <iostream>
 #include <filesystem>
 
-#if defined(RASPBERRY_PI) && RASPBERRY_PI
-std::string resolve_path(std::string file, const std::vector<std::string>& search_dirs) {
-  // Filepath construction priority
-  //  1. if path is an absolute path use as is
-  //  2. search of non-empty search paths
-  std::string fullpath = file;
-  std::vector<std::string> tested_paths;
-  bool file_found = false;
-
-  // Assume leading / means absolute path
-  if (file[0] == '/') {
-    file_found = std::filesystem::exists(fullpath);
-  } else {
-    for (const auto& search_dir : search_dirs) {
-      if (search_dir.empty()) continue;
-      fullpath = search_dir + "/" + file;
-      tested_paths.push_back(fullpath);
-      if (std::filesystem::exists(fullpath)) {
-        file_found = true;
-        break;
-      }
-    }
-  }
-
-  if (!file_found) {
-    std::cerr << "Failed to resolve location of file " << file << " tested:" << std::endl;
-    for (const auto& path : tested_paths)
-      std::cerr << "  " << path << std::endl;
-    exit(-1);
-  }
-
-  // Clean path string
-  return std::filesystem::path(fullpath).lexically_normal();
-}
-
-
-namespace sop {
-static std::string replace(
-  std::string str,
-  const std::string& from,
-  const std::string& to
-) {
-  size_t start_pos = 0;
-  while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-    str.replace(start_pos, from.length(), to);
-    start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-  }
-  return str;
-}
-
-static std::vector<std::string> split(std::string strToSplit, char delimeter)
-{
-  std::stringstream ss(strToSplit);
-  std::string item;
-  std::vector<std::string> splittedStrings;
-  while (std::getline(ss, item, delimeter))
-  {
-    splittedStrings.push_back(item);
-  }
-  return splittedStrings;
-}
-
-std::shared_ptr<NanoKernelMapping>
-read_pattern_mapping(const std::string& id, const std::vector<std::string>& search_dirs)
-{
-  std::string filepath = resolve_path("mapping_" + id + ".txt", search_dirs);
-
-  std::ifstream file(filepath);
-  std::string line;
-
-  if (!file.is_open()) {
-    std::cout << "Failed to open " << filepath << std::endl;
-    exit(-1);
-  }
-
-  std::getline(file, line);
-  int M_r = std::stoi(line);
-
-  auto pattern_mapping_ptr = std::make_shared<NanoKernelMapping>(1 << M_r);
-  auto& pattern_mapping = *pattern_mapping_ptr;
-
-  pattern_mapping[0].push_back(0);
-
-  while (std::getline(file, line)) {
-    // Cleanup
-    line = replace(line, "[", "");
-    line = replace(line, "]", "");
-
-    auto line_split = split(line, ':');
-    int pattern = std::stoi(line_split[0]);
-    auto nano_kernel_strings = split(line_split[1], ',');
-
-    // replace all 'x' to 'y'
-    for (const auto& nano_kernel_string : nano_kernel_strings) {
-      pattern_mapping[pattern].push_back(std::stoi(nano_kernel_string));
-    }
-  }
-
-  file.close();
-  return pattern_mapping_ptr;
-}
-}
-
-#endif
 
 extern "C" spnano_coo_t spnano_allocate_coo_matrix_f32(int rows, int cols) {
   return new COO<float>(rows, cols);
@@ -144,6 +40,7 @@ extern "C" void spnano_coo_matrix_add_nnz_f32(spnano_coo_t m, int i, int j, floa
   COO<float>* coo = reinterpret_cast<COO<float>*>(m);
   coo->append_nnz({i, j, val});
 }
+
 
 extern "C" spnano_matmul_t spnano_allocate_matmul_f32(spnano_coo_t m, int num_threads, int b_cols) {
   std::string mapping_id = "";
@@ -163,7 +60,7 @@ extern "C" spnano_matmul_t spnano_allocate_matmul_f32(spnano_coo_t m, int num_th
 
 
   sop::TileConfig tile_config;
-  sop::MatMul<float>* matmul;
+  sop::MatMul<float>* matmul = nullptr;
 
 #ifdef __AVX512VL__
   if (num_threads >= 1) {
@@ -185,84 +82,76 @@ extern "C" spnano_matmul_t spnano_allocate_matmul_f32(spnano_coo_t m, int num_th
     exit(-1);
   }
 
-  if (!packed) {
-    if (num_threads == 1) {
-      if (schedule == "KNM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatKNM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-      else if (schedule == "NKM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatNKM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-    } else {
-      if (schedule == "KNM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatLoadBalancedKNM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-      else if (schedule == "NKM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatLoadBalancedNKM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-    }
-  } else {
-    if (num_threads == 1) {
-      if (schedule == "KNM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatPackedKNM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-      else if (schedule == "NKM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatNKM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-    } else {
-      if (schedule == "KNM") {
-        matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatLoadBalancedKNM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-      else if (schedule == "NKM") {
-         matmul = new sop::MatMulSpecialized<sop::KD_IntelFloatLoadBalancedNKM>(
-          coo, b_cols, tile_config, num_threads, executor_id, mapping_id
-        );
-      }
-    }
-  }
-#elif defined(__ARM_NEON)
-  if (num_threads >= 1) {
-//      int N_c = 12;
-//      if (b_cols >= 1024) {
-//        N_c = 48;
-//      } else if (b_cols >= 512) {
-//        N_c = 12;
-//      } else if (b_cols >= 128) {
-//        N_c = 96;
-//      } else {
-//        N_c = 48;
-//      }
-//
-//      tile_config.M_c = coo->rows();
-//      tile_config.N_c = N_c;
-//      tile_config.K_c = (b_cols >= 1024) ? 64 : 128;
-//      tile_config.tiling_strategy = sop::MANUAL_TILING;
+#elif __AVX2__
 
-      tile_config.M_c = 8;
-      tile_config.N_c = 8;
-      tile_config.K_c = 32;
+  // TODO: Add AVX2 support
+
+#endif
+
+#if (defined(__AVX512F__) || defined(__AVX2__))
+
+#define CREATE_MATMUL(_packed, _load_balance_cond, _sechdule, KernelDesc)    \
+    if (packed == _packed && _load_balance_cond && schedule == _sechdule) {  \
+      ERROR_AND_EXIT_IF(matmul, "Already found matching kernel desc");       \
+      matmul = new sop::MatMulSpecialized<KernelDesc>(                       \
+         coo, b_cols, tile_config, num_threads, executor_id, mapping_id      \
+      ); \
+    }
+
+  CREATE_MATMUL(false, (num_threads == 1), "KNM", sop::KD_IntelFloatKNM);
+  CREATE_MATMUL(false, (num_threads == 1), "NKM", sop::KD_IntelFloatNKM);
+  CREATE_MATMUL(false, (num_threads >  1), "KNM", sop::KD_IntelFloatLoadBalancedKNM);
+  CREATE_MATMUL(false, (num_threads >  1), "NKM", sop::KD_IntelFloatLoadBalancedNKM);
+  CREATE_MATMUL(true,  (num_threads == 1), "KNM", sop::KD_IntelFloatPackedKNM);
+  // NOTE: NKM schedule or load balancing not supported for packed yet so use unpacked versions
+  CREATE_MATMUL(true,  (num_threads == 1), "NKM", sop::KD_IntelFloatNKM);
+  CREATE_MATMUL(true,  (num_threads >  1), "KNM", sop::KD_IntelFloatLoadBalancedKNM);
+  CREATE_MATMUL(true,  (num_threads >  1), "NKM", sop::KD_IntelFloatLoadBalancedNKM);
+
+#elif defined(__ARM_NEON)
+  if (num_threads == 1) {
+      int N_c = 12;
+      if (b_cols >= 1024) {
+        N_c = 48;
+      }
+      else if (b_cols >= 512) {
+        N_c = 12;
+      }
+      else if (b_cols >= 128) {
+        N_c = 96;
+      }
+      else {
+        N_c = 48;
+      }
+
+      tile_config.M_c = coo->rows();
+      tile_config.N_c = N_c;
+      tile_config.K_c = (b_cols >= 1024) ? 64 : 128;
+      tile_config.tiling_strategy = sop::MANUAL_TILING;
+
+      mapping_id = "61fee";
+      executor_id = "c22a5_NEON_128_4x2";
+      schedule = "N";
+  } else {
+      int N_c = 12;
+      if (b_cols >= 1024) {
+        N_c = 48;
+      } else if (b_cols >= 512) {
+        N_c = 12;
+      } else if (b_cols >= 128) {
+        N_c = 96;
+      } else {
+        N_c = 48;
+      }
+
+      tile_config.M_c = coo->rows();
+      tile_config.N_c = N_c;
+      tile_config.K_c = (b_cols >= 1024) ? 64 : 128;
       tile_config.tiling_strategy = sop::MANUAL_TILING;
 
       mapping_id = "61fee";
       executor_id = "c22a5_NEON_128_4x2";
       schedule = "M";
-  } else {
-    std::cout << "Not yet supported" << std::endl;
-    exit(-1);
   }
 
   if (schedule == "M") {
